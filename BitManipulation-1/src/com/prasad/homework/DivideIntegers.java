23e245d878c73dbae21bc18265a316b2a13a31e2
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2022-07-06 10:46:42 +0530

    Update DivideIntegers.java

--- BitManipulation-1/src/com/prasad/homework/DivideIntegers.java
+++ BitManipulation-1/src/com/prasad/homework/DivideIntegers.java
@@ -9,11 +9,26 @@ public class DivideIntegers {
     }
 
     public static int divide(int A, int B) {
-        int ans = 0;
-        while (A >= B && A > 0 && B != 0) {
-            A = A - B;
-            ans++;
-        }
-        return ans;
+        boolean negative = (A > 0 && B < 0) || (A < 0 && B > 0) ? true : false;
+
+    // I am casting A to long before doing abs because if value of A is Integer.MIN_VALUE which is -2 power 31
+    // the abs value will be 2 power 31 but max limit of int is 2 power 31 - 1
+    // So in this scenario, the abs function will return the same Integer.MIN_VALUE
+    long dividend = Math.abs((long)A);
+    long divisor = Math.abs((long)B);
+
+    long quotient = 0;
+    if (dividend == 0 || dividend < divisor) return 0;
+
+    for (int i = 31; i >= 0 ; i--) {
+      if ( dividend >= divisor << i) {
+        dividend -= divisor << i;
+        quotient = i == 31 ? quotient - (1 << i) : quotient + (1<<i);
+      }
+    }
+    if (negative) quotient = -quotient;
+    if (quotient > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
+    return (int) quotient;
     }
 }
