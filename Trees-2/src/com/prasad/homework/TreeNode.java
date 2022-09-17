ffb12aeb8483ddaca84b8ad13dbb06f21c295d53
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2022-09-17 22:23:51 +0530

    add tree node class

---
+++ Trees-2/src/com/prasad/homework/TreeNode.java
@@ -0,0 +1,14 @@
+package com.prasad.homework;
+
+public class TreeNode {
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+    left = null;
+    right = null;
+  }
+}
